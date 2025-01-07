4a49cd1c9c0379c3d16222d4139fb300d39950d8
saurabh kumar <sourabh21kr@gmail.com>
2025-01-07 15:06:03 +0530

    substraction added

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,9 @@ Class Calculator{
 public static int add(int a, int b){
 return a+b; 
 }
+public static int substract(int a, int b){
+return a-b; 
+}
 public static void main(String []args){
 System.out.println("saurabh");
 }
