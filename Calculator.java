cb1c786fee7bb73996840f03169078172559cf5d
saurabh kumar <sourabh21kr@gmail.com>
2025-01-07 15:20:33 +0530

    Division added

--- Calculator.java
+++ Calculator.java
@@ -8,6 +8,10 @@ return a-b;
 public static int multiply(int a, int b){
 return a*b;
 }
+public static int divide(int a, int b){
+return a/b;
+}
+
 public static void main(String []args){
 System.out.println("saurabh");
 }
