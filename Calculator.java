ca7776047522464475ce327901fb7ed275f8598c
saurabh kumar <sourabh21kr@gmail.com>
2025-01-07 15:14:02 +0530

    multiplication added

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,9 @@ return a+b;
 public static int substract(int a, int b){
 return a-b; 
 }
+public static int multiply(int a, int b){
+return a*b;
+}
 public static void main(String []args){
 System.out.println("saurabh");
 }
